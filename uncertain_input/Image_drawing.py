9fbdbacedb2fe865d7f621cd6416adcfd1980dd1
LeeXin <lixin2011@outlook.com>
2020-12-03 16:02:23 +0800

    添加了径向柱图的绘制功能，同时增加了渐变颜色列表

--- uncertain_input/Image_drawing.py
+++ uncertain_input/Image_drawing.py
@@ -1,14 +1,52 @@
-from matplotlib import colors
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.collections import PolyCollection
+import numpy as np
 import seaborn as sns
+from matplotlib import cm,colors
 
 os.chdir(r'D:\temp')
-df = pd.read_excel(r'flow.xlsx')
-
+df = pd.read_excel(r'8.xlsx')
+colors_green = [
+    '#edf8e9',
+    '#c7e9c0',
+    '#a1d99b',
+    '#74c476',
+    '#31a354',
+    '#006d2c'
+]
+colors_red = [
+    '#fee5d9',
+    '#fcbba1',
+    '#fc9272',
+    '#fb6a4a',
+    '#de2d26',
+    '#a50f15'
+]
+colors_blue = [
+    '#eff3ff',
+    '#c6dbef',
+    '#9ecae1',
+    '#6baed6',
+    '#3182bd',
+    '#08519c'
+]
+colors_purple = [
+    '#f2f0f7',
+    '#dadaeb',
+    '#bcbddc',
+    '#9e9ac8',
+    '#756bb1',
+    '#54278f'
+]
+colors_yellow = [
+    '#feedde'
+    '#fdd0a2',
+    '#fdae6b',
+    '#fd8d3c',
+    '#e6550d',
+    '#a63603'
+]
 def plot_samping_line_chart():
     colors = ['#377eb8','#984ea3','#ff7f00']
     for uncertainty_level in list(range(6)):
@@ -31,12 +69,39 @@ def plot_samping_hist_chart():
         for start_index,color_index in zip([0,5000,10000],[0,1,2]):
             y = df.iloc[start_index:start_index+5000,uncertainty_level]
             print(y)
-            plt.hist(y,color=colors[color_index],density=True,edgecolor='grey',bins=30)
+            plt.hist(y,color=colors[color_index],density=True,edgecolor='grey',bins=45)
         plt.xticks(family='Times New Roman',fontsize=17)
         plt.yticks(family='Times New Roman',fontsize=17)
         plt.ylim(0,2)
         plt.grid(axis='x')
         plt.savefig(str(uncertainty_level)+'-hist-chart.jpg')
+
+def plot_radial_column_chart():
+    plt.rc('patch',force_edgecolor=True)
+    plt.rc('axes',axisbelow=True)
+    fig = plt.figure(figsize=(8,8),dpi=300)
+    n_row = df.shape[0]
+    start_angle = 0
+    for i in range(1,9):
+        angle = np.arange(start_angle,start_angle + np.pi/4,np.pi/4/n_row)
+        # 将横坐标（行名）转换为角度坐标
+        radius = np.array(df.iloc[:,i])
+        # 选择不同的列
+        print(df.iloc[:,0])
+        # 将纵坐标（DATaFrame中的第一列）转换为半径坐标    
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+
+        plt.ylim(-0.03,0.2)
+        # 将起始点设置为负值，就可以获得一个空心内圆
+        plt.bar(angle,radius,color=colors_blue,alpha=0.9,width=0.13,align="edge", linewidth=0.2,edgecolor='k',zorder=1)
+        # width需要微调保证比例，避免重叠
+        start_angle += np.pi/4
+    plt.grid(which='major',axis ="x", linestyle='-', linewidth='0.5', color='gray',alpha=0.5,zorder=1)
+    plt.grid(which='major',axis ="y", linestyle='-', linewidth='0.5', color='gray',alpha=0.5,zorder=1)
+    plt.xticks(alpha=0)
+    plt.yticks(alpha=0)
+    plt.savefig(r'1.jpg')
+
 
 def plot_waterfall_3D():
     fig = plt.figure(figsize=(8,8),dpi=200)
@@ -108,4 +173,5 @@ def scatter():
     plt.savefig(r'3.jpg')
 
 # plot_samping_line_chart()
-plot_samping_hist_chart()
+# plot_samping_hist_chart()
+plot_radial_column_chart()
