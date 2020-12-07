217c75842421c1a56a6ba75af574d68eab7303ef
LeeXin <lixin2011@outlook.com>
2020-12-07 18:34:10 +0800

    添加了哑铃图的绘制功能

--- uncertain_input/Image_drawing.py
+++ uncertain_input/Image_drawing.py
@@ -3,17 +3,15 @@ import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from matplotlib import cm,colors
 
 os.chdir(r'D:\temp')
-df = pd.read_excel(r'8.xlsx')
 colors_green = [
-    '#edf8e9',
-    '#c7e9c0',
     '#a1d99b',
     '#74c476',
-    '#31a354',
-    '#006d2c'
+    '#41ab5d',
+    '#238b45',
+    '#006d2c',
+    '#00441b'
 ]
 colors_red = [
     '#fee5d9',
@@ -24,30 +22,33 @@ colors_red = [
     '#a50f15'
 ]
 colors_blue = [
-    '#eff3ff',
-    '#c6dbef',
     '#9ecae1',
     '#6baed6',
-    '#3182bd',
-    '#08519c'
+    '#4292c6',
+    '#2171b5',
+    '#08519c',
+    '#08306b'
 ]
 colors_purple = [
-    '#f2f0f7',
-    '#dadaeb',
     '#bcbddc',
     '#9e9ac8',
-    '#756bb1',
-    '#54278f'
+    '#807dba',
+    '#6a51a3',
+    '#54278f',
+    '#3f007d'
 ]
 colors_yellow = [
-    '#feedde'
-    '#fdd0a2',
     '#fdae6b',
     '#fd8d3c',
-    '#e6550d',
-    '#a63603'
+    '#f16913',
+    '#d94801',
+    '#a63603',
+    '#7f2704'
 ]
 def plot_samping_line_chart():
+    '''
+    绘制采样点的柱形图
+    '''
     colors = ['#377eb8','#984ea3','#ff7f00']
     for uncertainty_level in list(range(6)):
         plt.figure(figsize=(9,5),dpi=300)
@@ -63,6 +64,9 @@ def plot_samping_line_chart():
         plt.savefig(str(uncertainty_level)+'-line-chart.jpg')
 
 def plot_samping_hist_chart():
+    '''
+    绘制采样点的直方图
+    '''
     colors = ['#377eb8','#984ea3','#ff7f00']
     for uncertainty_level in list(range(6)):
         plt.figure(figsize=(9,5),dpi=300)
@@ -77,6 +81,9 @@ def plot_samping_hist_chart():
         plt.savefig(str(uncertainty_level)+'-hist-chart.jpg')
 
 def plot_radial_column_chart():
+    '''
+    绘制故障的径向柱图
+    '''
     plt.rc('patch',force_edgecolor=True)
     plt.rc('axes',axisbelow=True)
     fig = plt.figure(figsize=(8,8),dpi=300)
@@ -90,7 +97,6 @@ def plot_radial_column_chart():
         print(df.iloc[:,0])
         # 将纵坐标（DATaFrame中的第一列）转换为半径坐标    
         ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
-
         plt.ylim(-0.03,0.2)
         # 将起始点设置为负值，就可以获得一个空心内圆
         plt.bar(angle,radius,color=colors_blue,alpha=0.9,width=0.13,align="edge", linewidth=0.2,edgecolor='k',zorder=1)
@@ -102,76 +108,38 @@ def plot_radial_column_chart():
     plt.yticks(alpha=0)
     plt.savefig(r'1.jpg')
 
-
-def plot_waterfall_3D():
-    fig = plt.figure(figsize=(8,8),dpi=200)
-    ax = fig.gca(projection='3d')
-    ax.view_init(azim=-70,elev=20)
-    ax.grid(False)
-    xs = ['0','0.2','0.4','0.6','0.8','1.0']
-    zs = ['PRE','PRC','kW']
-    verts = []
-    for z in zs:
-        ys = df[z].values
-        verts.append(list(zip(xs,ys)))
-    print(verts)
-    pal_husl = sns.husl_palette(len(zs),h=15/360,l=0.65,s=1).as_hex
-    # 在HUSL调色空间获得均匀间隔的颜色
-    poly = PolyCollection(verts,facecolors=pal_husl,edgecolor='k')
-    # ax.add_collection3d(poly,zs=zs,zdir='y')
-    # plt.show()
-
-def plot_3():
-    plt.figure(figsize=(23,6),dpi=300)
-    x = df.columns[1:]
-    color = ['#d0d1e6','#c6dbef','#9ecae1','#6baed6','#3182bd','#08519c']
-    for var,mc in zip(list(range(0,5)),[1,2,3,4,5]):
-        y = df.iloc[var,1:].values
-        print(y)
-        plt.bar(x=x,height=y,edgecolor='k',color=color[mc],zorder=10-var,width=0.8)
+def plot_dumbball_chart(df,var,colors,w,h,start):
+    '''
+    绘制变量的哑铃图
+    '''
+    plt.rcParams["axes.grid"] = True
+    plt.rcParams["grid.linestyle"] = (10,10)
+    df = df.sort_values(axis=1,by=[1.0])
+    print(df)
+    fig = plt.figure(figsize=(w,h),dpi=300)
+    y = df.columns
+    for i in range(6): # 指定不确定度
+        x = df.iloc[i,:]
         print(x)
-
-    plt.xticks(family='Times New Roman',fontsize=15)
-    plt.yticks(family='Times New Roman',fontsize=15)
-    plt.ylim(0,30)
-    # plt.fill_between(x,df['kW'],0,color=color[5],alpha=0.3,zorder=1)
-    # plt.fill_between(x,df['PRC'],0,color=color[4],alpha=0.3,zorder=2)
-    # plt.fill_between(x,df['PRE'],0,color=color[3],alpha=0.3,zorder=3)
-    plt.savefig(r'3.jpg')
-
-def plot_8():
-    plt.figure(figsize=(8,4),dpi=300)
-    # x = ['0','0.2','0.4','0.6','0.8','1.0']
-    color = [  '#66c2a5',
-               '#fc8d62',
-               '#8da0cb',
-               '#e78ac3',
-               '#a6d854', ]
-
-    for var,mc in zip([0,5000,10000],list(range(3))):
-        y = df.iloc[var:var+5000,5]
         print(y)
-        plt.hist(y,zorder=5-mc,label=var,color=color[mc],bins=50,edgecolor='grey',density=True)
-    plt.xlim(260,275)
-    plt.ylim(0,2)
-    plt.xticks(family='Times New Roman',fontsize=15)
-    plt.yticks(family='Times New Roman',fontsize=15)
-    # plt.fill_between(x,df['kW'],0,color=color[5],alpha=0.3,zorder=1)
-    # plt.fill_between(x,df['PRC'],0,color=color[4],alpha=0.3,zorder=2)
-    # plt.fill_between(x,df['PRE'],0,color=color[3],alpha=0.3,zorder=3)
-    plt.savefig(r'8.jpg')
-
-def scatter():
-    plt.figure(figsize=(8,4),dpi=300)
-    x = df.index
-    y = df.iloc[:,5]
-    plt.plot(x,y,linewidth=1,color='#08306b')
-    plt.xticks(family='Times New Roman',fontsize=15)
-    plt.yticks(family='Times New Roman',fontsize=15)
-    plt.ylim(260,275)
-    plt.grid(axis='y')
+        plt.scatter(x,y,color=colors[5-i],zorder=1)
+    for i in range(5): # 指定不确定度，需要减1
+        for j in range(var): # 指定变量
+            plt.hlines(y=df.columns[j],xmin=df.iloc[i,j],xmax=df.iloc[i+1,j],zorder=0,colors=colors[5-i])
+    plt.xticks([0.98,1.0],family='Times New Roman',fontsize=18)
+    plt.yticks(family='Times New Roman',fontsize=18)
+    # plt.grid(axis='x',alpha=0.5)
     plt.savefig(r'3.jpg')
 
-# plot_samping_line_chart()
-# plot_samping_hist_chart()
-plot_radial_column_chart()
+
+
+
+if __name__ == "__main__":
+    df = pd.read_excel(r'36.xlsx',index_col='std')
+    # plot_samping_line_chart()
+    # plot_samping_hist_chart()
+    # plot_radial_column_chart()
+    # plot_dumbball_chart(df,3,colors_green,5,6,0.5)
+    # plot_dumbball_chart(df,8,colors_blue,7,9,0.85)
+    # plot_dumbball_chart(df,22,colors_purple,9,16,0.9)
+    plot_dumbball_chart(df,36,colors_yellow,9,22,0.9)
